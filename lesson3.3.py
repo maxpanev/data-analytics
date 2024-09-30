d576bd89467663b4f8d36b0d9a8dc5efac2f3e4a
maxpanev <maxpanev170100@gmail.com>
2024-09-30 16:32:08 +0300

    add new files

---
+++ lesson3.3.py
@@ -0,0 +1,33 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome()
+
+url = "https://www.divan.ru/krasnodar/category/divany-i-kresla"
+
+driver.get(url)
+
+time.sleep(3)
+
+divans = driver.find_elements(By.CLASS_NAME, '_Ud0k')
+
+my_list = []
+
+for divan in divans:
+    try:
+        name = divan.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
+        price = divan.find_element(By.CSS_SELECTOR, 'span.ui-LD-ZU').text
+        my_list.append([name, price])
+    except NoSuchElementException:
+        print("Произошла ошибка при парсинге")
+        continue
+
+driver.quit()
+
+with open("homework.csv", 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Название', 'Цена'])
+    writer.writerows(my_list)
